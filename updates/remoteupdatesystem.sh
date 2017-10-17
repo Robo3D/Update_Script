9f32c02e4726fa4c1aad0effc5645d7867301c81
Victor E <victorevector@gmail.com>
2017-10-17 12:27:03 -0700

    better loggging... lets see whats goingon

--- updates/remoteupdatesystem.sh
+++ updates/remoteupdatesystem.sh
@@ -1,38 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
 THIS_DIR=$(echo $(pwd))
 RRUS_DIR=$THIS_DIR/../roboRemoteUpdateSystem
 USER_PI="sudo -u pi"
+LOG=/home/pi/SHINFO.txt
 
-echo "Cloning Repository..."
-git clone https://robo3dtest:r0b0r0b0@github.com/Robo3D/roboRemoteUpdateSystem.git $RRUS_DIR
+echo "Cloning Repository..." &>> $LOG
+git clone https://robo3dtest:r0b0r0b0@github.com/Robo3D/roboRemoteUpdateSystem.git $RRUS_DIR &>> $LOG
 
-echo "Running installation..."
-cd $RRUS_DIR
-chmod +x install.sh
-./install.sh
+echo "Running installation..." &>> $LOG
+cd $RRUS_DIR &>> $LOG
+chmod +x install.sh &>> $LOG
+./install.sh &>> $LOG
 
-echo "Starting roboRemoteUpdateSystem in 15 seconds..."
-sudo service roboRemoteUpdateSystem restart
+echo "Starting roboRemoteUpdateSystem in 15 seconds..." &>> $LOG
+sudo service roboRemoteUpdateSystem restart &>> $LOG
 # give RRUS time to start up
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
   sleep 1
-  echo $i
+  echo $i &>> $LOG
 done
 
-echo "Running playbook..."
-$USER_PI /home/pi/oprint/bin/python $THIS_DIR/../assets/run_playbook.py
+cd $THIS_DIR &>> $LOG
+echo "Running playbook..." &>> $LOG
+chmod +x $THIS_DIR/../assets/run_playbook.py &>> $LOG
+$USER_PI /home/pi/oprint/bin/python $THIS_DIR/../assets/run_playbook.py &>> $LOG
 if [ $? -eq 0 ]; then
-  echo "successfully run_playbook.py" >> /home/pi/SHINFO.txt
+  echo "successfully run_playbook.py" &>> $LOG
 else
-  echo "unsuccessfully run_playbook.py" >> /home/pi/SHINFO.txt
-  echo $VAL >> /home/pi/SHINFO.txt
+  echo "unsuccessfully run_playbook.py" &>> $LOG
   exit 1
 fi
 
-echo "Finishing up..."
+echo "Finishing up..." &>> $LOG
 #store that the update has occured
 echo ${0##*/} >> /home/pi/.updates.txt
-echo "Update Complete!"
+echo "Update Complete!" >> $LOG
 exit 0
