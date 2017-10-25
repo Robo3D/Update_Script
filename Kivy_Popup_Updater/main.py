c35399a5efce39f869a4ea8080db4270814c3f0b
Victor E <victorevector@gmail.com>
2017-10-25 12:37:05 -0700

    Revert "Testing: does it fail with old KV file?"
    This reverts commit e1189bbe3e1d0a12941ac873b9d65623ce976e47.

--- Kivy_Popup_Updater/main.py
+++ Kivy_Popup_Updater/main.py
@@ -38,7 +38,10 @@ from kivy.core.window import Window
 from kivy.clock import Clock
 import subprocess
 
-from popup_screen import Updating_Popup
+from popup_screen import Updating_Popup, USB_Progress_Popup
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+resource_add_path(DIR_PATH)
 
 class Updater_Screen_Manager(ScreenManager):
     def __init__(self,**kwargs):
@@ -49,8 +52,7 @@ class Updater_Screen_Manager(ScreenManager):
 class MainScreen(Screen):
     def __init__(self,**kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        poppy = Updating_Popup()
-
+        poppy = USB_Progress_Popup()
 
 
 class Updater_App(App):
@@ -58,13 +60,14 @@ class Updater_App(App):
         super(Updater_App, self).__init__(*args, **kwargs)
         self.update_progress = kwargs['progress']
         Logger.info('THIS IS THE DIR_PATH::: {}'.format(DIR_PATH))
-        Logger.info('THIS IS THE PROGRESS OBJ::: {}'.format(self.update_progress))
-
 
     def build(self):
-        Logger.info("Updater_App.build()!")
-        sm = Builder.load_file('updater.kv')
-        Logger.info("Opening Up Update Screen")
-        return sm
-
-Updater_App().run()
+        try:
+            Logger.info('Build called...')
+            Logger.info('Loading file: {}'.format(DIR_PATH+'/updater.kv'))
+            sm = Builder.load_file(DIR_PATH+'/updater.kv')
+            Logger.info("Opening Up Update Screen")
+        except Exception as e:
+            Logger.info(str(e))
+        else:
+            return sm
