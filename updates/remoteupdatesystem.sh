198addffa5192beb9a1af51e58786246654ffad7
Victor E <victorevector@gmail.com>
2017-11-07 16:36:11 -0800

    better error handling. ensure that all components of this script get executed.

--- updates/remoteupdatesystem.sh
+++ updates/remoteupdatesystem.sh
@@ -5,19 +5,35 @@ RRUS_DIR=$THIS_DIR/../roboRemoteUpdateSystem
 USER_PI="sudo -u pi"
 LOG=/home/pi/SHINFO.txt
 
-echo "Cloning Repository..." &>> $LOG
-git clone https://robo3dtest:r0b0r0b0@github.com/Robo3D/roboRemoteUpdateSystem.git $RRUS_DIR &>> $LOG
+verify_success () {
+  if [ $1 -eq 0 ]; then
+    echo "$2 ...success" &>> $LOG
+  else
+    echo "$2 ...failure" &>> $LOG
+    exit 1
+  fi
+}
 
-echo "Running installation..." &>> $LOG
+task="[Task]:: Cloning Repository..."
+echo $task &>> $LOG
+git clone https://robo3dtest:r0b0r0b0@github.com/Robo3D/roboRemoteUpdateSystem.git $RRUS_DIR &>> $LOG
+verify_success $? $task
+
+task="[Task]:: Running installation..."
+echo $task &>> $LOG
 cd $RRUS_DIR &>> $LOG
 chmod +x install.sh &>> $LOG
 ./install.sh &>> $LOG
+verify_success $? $task
 
-echo "Install compliant RoboLCD version..." &>> $LOG
+task="[Task]:: Install compliant RoboLCD version..."
+echo $task &>> $LOG
 cd $THIS_DIR &>> $LOG
 $USER_PI /home/pi/oprint/bin/pip install https://github.com/victorevector/RoboLCD/archive/RRUSpub.zip
+verify_success $? $task
 
-echo "Starting roboRemoteUpdateSystem in 15 seconds..." &>> $LOG
+task="[Task]:: Starting roboRemoteUpdateSystem in 15 seconds..."
+echo $task &>> $LOG
 sudo service roboRemoteUpdateSystem restart &>> $LOG
 # give RRUS time to start up
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
@@ -27,15 +43,11 @@ do
 done
 
 cd $THIS_DIR &>> $LOG
-echo "Running playbook..." &>> $LOG
+task="[Task]:: Running playbook..."
+echo $task &>> $LOG
 chmod +x $THIS_DIR/../assets/run_playbook.py &>> $LOG
 $USER_PI /home/pi/oprint/bin/python $THIS_DIR/../assets/run_playbook.py &>> $LOG
-if [ $? -eq 0 ]; then
-  echo "successfully run_playbook.py" &>> $LOG
-else
-  echo "unsuccessfully run_playbook.py" &>> $LOG
-  exit 1
-fi
+verify_success $? $task
 
 echo "Finishing up..." &>> $LOG
 #store that the update has occured
