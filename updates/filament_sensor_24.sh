afd5ced4014e4995515adade18fdd4faff556132
Victor E <victorevector@gmail.com>
2017-12-21 16:28:25 -0800

    added verification

--- updates/filament_sensor_24.sh
+++ updates/filament_sensor_24.sh
@@ -3,9 +3,21 @@ HOME_DIR="/home/pi"
 VENV="oprint"
 USER_PI="sudo -u pi"
 THIS_DIR=`dirname $0`
+LOG=/home/pi/.octoprint/logs/update_info.txt
 
+verify_success () {
+  if [ $1 -eq 0 ]; then
+    echo "$2 ...success" &>> $LOG
+  else
+    echo "$2 ...failure" &>> $LOG
+    exit 1
+  fi
+}
 # Detect filament installation status
+task="[Task]:: Installing filament sensor..."
+echo $task &>> $LOG
 $USER_PI $HOME_DIR/$VENV/bin/python $THIS_DIR/../assets/upgrade_pkg.py "Filament-Sensor" "https://github.com/Robo3D/Octoprint-Filament/archive/2.4.zip"
+verify_success $? $task
 
 
 #complete the update
