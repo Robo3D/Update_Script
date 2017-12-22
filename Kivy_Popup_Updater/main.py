abb31b585ad2d7af79b40336f64d6db3165ed943
Victor E <victorevector@gmail.com>
2017-12-21 18:20:13 -0800

    Gui responds to update failures + progress bar

--- Kivy_Popup_Updater/main.py
+++ Kivy_Popup_Updater/main.py
@@ -19,7 +19,14 @@ Config.set('kivy', 'keyboard_mode', 'dock')
 Config.set('graphics', 'height', '320')
 Config.set('graphics', 'width', '480')
 Config.set('graphics', 'borderless', '1')
-Config.set('input', '%(name)s', 'probesysfs,provider=hidinput,param=rotation=270,param=invert_y=1')
+from detect_model import detect_model
+model = detect_model('/home/pi/.octoprint/config.yaml')
+if model == 'c2hdmi':
+    Config.set('input', '%(name)s', 'probesysfs,provider=hidinput,param=rotation=180,param=invert_y=1,param=invert_x=1')
+elif model == 'c2':
+    Config.set('input', '%(name)s', 'probesysfs,provider=hidinput,param=rotation=270,param=invert_y=1')
+else:
+    Config.set('input', '%(name)s', 'probesysfs,provider=hidinput,param=invert_x=1')
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -38,7 +45,10 @@ from kivy.core.window import Window
 from kivy.clock import Clock
 import subprocess
 
-from popup_screen import Updating_Popup
+from popup_screen import USB_Progress_Popup, UpdateStats, Failed_Updating_Popup, Restarting_Popup
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+resource_add_path(DIR_PATH)
 
 class Updater_Screen_Manager(ScreenManager):
     def __init__(self,**kwargs):
@@ -49,16 +59,40 @@ class Updater_Screen_Manager(ScreenManager):
 class MainScreen(Screen):
     def __init__(self,**kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        poppy = Updating_Popup()
-        
+        self.prog_poppy = USB_Progress_Popup()
+        self.prog_poppy.open()
+        self.failed_poppy = Failed_Updating_Popup()
+        self.check_stats = Clock.schedule_interval(self.update, 0.5)
 
+    def update(self,dt):
+        is_complete = int(self.prog_poppy.current_percent) is 1
+        any_failures = self.prog_poppy.update_stats.failed > 0
+        if is_complete:
+            Logger.info("Update completed...")
+            if any_failures:
+                Logger.info("...Failed detected!")
+                self.failed_poppy.open()
+                self.prog_poppy.dismiss()
+                Clock.unschedule(self.check_stats)
+            else:
+                Logger.info("...No failed detected!")
+                r = Restarting_Popup()
+                r.open()
+                self.prog_poppy.dismiss()
+            Clock.unschedule(self.prog_poppy.sched_update)
 
 class Updater_App(App):
 
     def build(self):
+        try:
+            Logger.info('Build called...')
+            Logger.info('Loading file: {}'.format(DIR_PATH+'/updater.kv'))
+            sm = Builder.load_file(DIR_PATH+'/updater.kv')
+            Logger.info("Opening Up Update Screen")
+        except Exception as e:
+            Logger.info(str(e))
+        else:
+            return sm
 
-        sm = Builder.load_file('updater.kv')
-        print("Opening Up Update Screen")
-        return sm
-
-Updater_App().run()
+if __name__ == '__main__':
+    Updater_App().run()
