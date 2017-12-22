895e239f41be6594634da2dc8f10c51f8d92e58c
Victor E <victorevector@gmail.com>
2017-12-21 16:25:10 -0800

    verify expandfs

--- updates/ensure_expandedfs.sh
+++ updates/ensure_expandedfs.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+LOG=/home/pi/.octoprint/logs/update_info.log
+verify_success () {
+  if [ $1 -eq 0 ]; then
+    echo "$2 ...success" &>> $LOG
+  else
+    echo "$2 ...failure" &>> $LOG
+    exit 1
+  fi
+}
 
+task="[Task]:: Expand fs..."
+echo $task &>> $LOG
 sudo /usr/bin/raspi-config --expand-rootfs
+verify_success $? $task
 
 #store that the update has occured
 echo ${0##*/} >> /home/pi/.updates.txt
