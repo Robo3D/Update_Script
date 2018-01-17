c48568fbf69d5962cb1e786e9359dbd3b7a82a57
Ximidar <ximidar@gmail.com>
2018-01-17 09:00:42 -0800

    Added refresher to announcements

--- updates/security_100.sh
+++ updates/security_100.sh
@@ -2,6 +2,7 @@
 THIS_DIR=`dirname $0`
 ACL_DIRECTORY="/home/pi/.octoprint/data/roboACL"
 SSH_STATE_PATH="/home/pi/.octoprint/ssh_marker.txt"
+ANNOUNCEMENT_FOLDER='/home/pi/.octoprint/data/announcements'
 HOME_DIR="/home/pi"
 VENV="oprint"
 OCTO_DIR=$HOME_DIR/OctoPrint
@@ -26,10 +27,11 @@ sudo chmod 0755 $ACL_DIRECTORY/roboacl.py
 #change ssh state to disabled
 systemctl disable ssh && touch $SSH_STATE_PATH
 
-
-
 #add the custom commands to the system tray
 python_run $THIS_DIR/../assets/security_assets/yedit.py
+
+#refresh announcements by deleting the announcements
+sudo rm -r $ANNOUNCEMENT_FOLDER/*
 
 #store that the update has occured
 echo ${0##*/} >> /home/pi/.updates.txt
