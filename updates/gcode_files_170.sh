418d6ce07d4a1354b47f291a83f7d289d8c62374
Ximidar <ximidar@gmail.com>
2018-01-16 16:15:08 -0800

    Updating the gcode workflow

--- updates/gcode_files_170.sh
+++ updates/gcode_files_170.sh
@@ -6,9 +6,10 @@ UPLOADS="/home/pi/.octoprint/uploads"
 echo "adding new gcode files"
 
 #download .zip folder of assets
-cd $THIS_DIR/../assets/gcode_files/
-wget https://s3.amazonaws.com/roboupdate/assets/c2_gcode_files.zip
+GCODE_FOLDER=$THIS_DIR/../assets/gcode_files/c2_gcode_files.zip
+wget -O $GCODE_FOLDER https://s3.amazonaws.com/roboupdate/assets/c2_gcode_files.zip
 
+cd $THIS_DIR/../assets/gcode_files/
 #unzip all files to the uploads folder
 if [ -f "c2_gcode_files.zip" ]; then
     $USER_PI unzip -o c2_gcode_files.zip -d $UPLOADS/ 
